bb397a3678dc315d16029259373c0a60114576e2
dch23 <dcvweb123@gmail.com>
2020-09-04 11:59:06 -0400

    player movement

---
+++ Game One/Assets/Scripts/Player movement/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float speed = 30f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // set rigid body
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddForce(transform.parent.transform.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb.AddForce(-transform.parent.transform.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.AddForce(transform.parent.transform.right * speed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rb.AddForce(-transform.parent.transform.right * speed);
+        }
+    }
+}
